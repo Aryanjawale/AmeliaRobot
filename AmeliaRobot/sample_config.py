9e2520a5c9b80f271a9d3fad1ab1696148902f44
Aryan Jawale <87902607+Aryanjawale@users.noreply.github.com>
2022-01-04 22:08:21 +0530

    Update sample_config.py

--- AmeliaRobot/sample_config.py
+++ AmeliaRobot/sample_config.py
@@ -13,38 +13,34 @@ class Config(object):
     LOGGER = True
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
-
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+ 
+    API_ID = "4759912"  # integer value, dont use ""
+    API_HASH = "5bcc548f108e30a913a339a4f97f315c"
+    TOKEN = "5059068789:AAG1p9zixkDPEUZCjlKOz1g-OjPWaH1Xx0g"
+    OWNER_ID = 1323719429  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Aryanjawale"
+    SUPPORT_CHAT = "trainer_zone"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001197394366
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001197394366
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:EhwlE3xkvmP7cIAQeAIS@containers-us-west-21.railway.app:6676/railway"
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    BOT_USERNAME = "@HawkMoth_rbot"
+    SPAMWATCH_API = "M7r11AlyDZGvFh2F9U6umCS96iVvyMC~Bo9khw2cKMcYvRBYyDOUNJ7FLQqEEwgP"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
-    # OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
     DRAGONS = get_user_list("elevated_users.json", "sudos")
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
     DEV_USERS = get_user_list("elevated_users.json", "devs")
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     DEMONS = get_user_list("elevated_users.json", "supports")
-    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list("elevated_users.json", "tigers")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = None  # EG, paypal
@@ -52,21 +48,41 @@ class Config(object):
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
+    REM_BG_API_KEY = "dxsh728mZMDmj4ijSZCNPZig"
+    GENIUS_API_TOKEN = "LstbAM4nzj2txUk2DGKh-PC0z6BxY1JYhMxMdj1NdRtmm7B7XYs0WcYBj4bFLK"
+    MONGO_DB_URI = "mongodb://mongo:iFlA5trbE3FpYAdasgdP@containers-us-west-25.railway.app:6867"
+    STRING_SESSION = "1BQANOTEuMTA4LjU2LjEzMAG7Y0dLVFQdbcP+HCrwb3LpkJeNtVWxbcndcHx24mQTDzjwAyqZ719yAwbJAomLHam9mYfViV2cA0Lfxao6mlOnnMBfJIE3ReEdUW+QlFC1LYp6k4prFaMVJZUzKpx2NCd1a7oeXNSQ9AHGzIIXcYCh5Yp2Y2/BE5T9R6z+tNKJ2bj8NCz/LGmLkzZeZmo+UKYO5liTd+gSLdXH5AyeO75dC4rbPAXV7jMsOauQq/c2p+BJBmWaI4C7fE2kp9TkBTOseZDviSxNEt0ADkXBJGw56Pa3L/bDNR9CHS6rn8V+JlY2N5/UXaspsPjFPwSFf5UQNQ1plx8PqNj2AGFYPnGn6Q=="
+    STRICT_GMUTE = True
+    BOT_USERNAME = "@HawkMoth_rbot"
+    ARQ_API_URL = "https://thearq.tech/" 
+    BOT_NAME = "HᴀᴡᴋMᴏᴛʜ"
+    ARQ_API_KEY = "CEWQIX-PODJTL-RNSXWN-GBRPKV-ARQ"
+    LOG_GROUP_ID = "-1001197394366"
+    OPENWEATHERMAP_ID = "e8c43576833a8867d24a4e6785349e20"
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    ERROR_LOGS = "-1001197394366"
+    LASTFM_API_KEY = "73d42d9c93626709dc2679d491d472bf"
+    BOT_ID = "5059068789"
+    WELCOME_DELAY_KICK_SEC = "5"
+    REDIS_URL = "redis://Zaidrobot:Anmol@123@redis-12356.c284.us-east1-2.gce.cloud.redislabs.com:12356/Zaidrobot"
+    
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    BAN_STICKER = "CAADBQAD6QQAAsMSmVU8Rp977RgWBQI"  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True
+    ALLOW_CHATS = True  
     CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "V7NS1NBFEL4X24L6"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "2AS711XS1O9B"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
-    SPAMMERS = None
+    SPAMMERS = None # .
+
 
 class Production(Config):
     LOGGER = True
